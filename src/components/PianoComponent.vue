da820123f48a77d41580b0ff309403ba21d3a8dd
Claudia Matosa <claudia.matosa@masabi.com>
2023-03-30 19:28:53 +0100

    Working piano

--- src/components/PianoComponent.vue
+++ src/components/PianoComponent.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
-const name: string = "World";
+import * as Tone from "tone";
+// import { ref } from 'vue';
+
+const keys = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const synth = new Tone.Synth().toDestination();
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
+const playNote = (note: string) => {
+  synth.triggerAttackRelease(`${note}4`, "8n");
+};
 </script>
 
 <template>
-  <div>Hello {{ name }}!</div>
+  <div>
+    <h1>Piano</h1>
+    <button v-for="key in keys" v-bind:key="key" @click="playNote(key)">{{ key }}</button>
+  </div>
 </template>
+
+<style scoped>
+h1 {
+  color: var(--color-text);
+}
+</style>
